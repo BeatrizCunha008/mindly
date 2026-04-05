bc9146656da585645ec3db3560486087fe701ff4
BeatrizCunha008 <5482@eclisboa.net>
2026-04-05 20:34:50 +0100

    fix csrf

--- mindly_project/settings.py
+++ mindly_project/settings.py
@@ -134,3 +134,5 @@ DATABASES['default'] = dj_database_url.config(
     default=f'sqlite:///{BASE_DIR / "db.sqlite3"}',
     conn_max_age=600
 )
+
+CSRF_TRUSTED_ORIGINS = ['https://web-production-a7d7.up.railway.app']
