25045930c685a055ae999203b74b19aa4b6e3e54
BeatrizCunha008 <5482@eclisboa.net>
2026-05-23 16:53:38 +0100

    CSRF2

--- mindly_project/settings.py
+++ mindly_project/settings.py
@@ -124,12 +124,7 @@ DATABASES['default'] = dj_database_url.config(
     conn_max_age=600
 )
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 465
-EMAIL_USE_TLS = False
-EMAIL_USE_SSL = True
-
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
-DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
+EMAIL_BACKEND = 'sendgrid_backend.SendgridBackend'
+SENDGRID_API_KEY = os.environ.get('SENDGRID_API_KEY')
+DEFAULT_FROM_EMAIL = '5482@eclisboa.net'
+SENDGRID_SANDBOX_MODE_IN_DEBUG = False
