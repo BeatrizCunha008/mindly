f3bf7c36f97b8b09193f8b5e822f831e2ce7bbbd
BeatrizCunha008 <5482@eclisboa.net>
2026-05-23 17:39:10 +0100

    CSRF2

--- mindly_project/settings.py
+++ mindly_project/settings.py
@@ -47,7 +47,7 @@ ROOT_URLCONF = 'mindly_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'core' / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
