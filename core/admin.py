da0044b7a7be2f02fe115aa65fdfa6b29efa9c68
BeatrizCunha008 <5482@eclisboa.net>
2026-05-24 12:17:28 +0100

    CSRF2

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin
+from .models import Perfil, RelacaoPsicologoPaciente, RegistoHumor, EntradaDiario, FotoAlbum
 
-# Register your models here.
+class PerfilInline(admin.StackedInline):
+    model = Perfil
+    can_delete = False
+
+class CustomUserAdmin(UserAdmin):
+    inlines = [PerfilInline]
+
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
+admin.site.register(RelacaoPsicologoPaciente)
+admin.site.register(RegistoHumor)
+admin.site.register(EntradaDiario)
+admin.site.register(FotoAlbum)
