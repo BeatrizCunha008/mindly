784aa9fd7d005e537385069c5c14b25d04fb55eb
BeatrizCunha008 <5482@eclisboa.net>
2026-05-24 11:12:05 +0100

    CSRF2

--- mindly_project/urls.py
+++ mindly_project/urls.py
@@ -5,6 +5,18 @@ from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('core.urls')),
+    path('i18n/', include('django.conf.urls.i18n')),
+    path('rosetta/', include('rosetta.urls')),
+    path('password-change/', auth_views.PasswordChangeView.as_view(
+        template_name='core/password_change.html',
+        success_url='/password-change/done/'
+    ), name='password_change'),
+    path('password-change/done/', auth_views.PasswordChangeDoneView.as_view(
+        template_name='core/password_change_done.html'
+    ), name='password_change_done'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='login', http_method_names=['get', 'post']), name='logout'),
     path('password-reset/', auth_views.PasswordResetView.as_view(
         template_name='registration/password_reset_form.html'
     ), name='password_reset'),
